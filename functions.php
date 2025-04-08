eeaa2bbbcb68bc983e2bdc9e24d70fa15711f3b9
Yuki Mizutani <y.mizutani2005@gmail.com>
2025-04-08 11:58:03 +0900

    Fix some view functions error
    閲覧数の更新ができておらず、人気記事ランキングが機能していなかったエラーを修正

--- functions.php
+++ functions.php
@@ -10,29 +10,38 @@ function myTheme_enqueue_style_script() {
 /* 人気記事一覧
 ---------------------------------------------------------- */
 //記事閲覧数を取得する
-function getPostViews($postID){
-    $count_key = 'post_views_count';
-    $count = get_post_meta($postID, $count_key, true);
-    if($count==''){
-      delete_post_meta($postID, $count_key);
-      add_post_meta($postID, $count_key, '0');
-      return "0 View";
-    }
-    return $count.' Views';
-  }
-  //記事閲覧数を保存する
-  function setPostViews($postID) {
-    $count_key = 'post_views_count';
-    $count = get_post_meta($postID, $count_key, true);
-    if($count==''){
+function getPostViews($postID) {
+  $count_key = 'post_views_count';
+  $count = get_post_meta($postID, $count_key, true);
+  if ($count == '') {
       $count = 0;
       delete_post_meta($postID, $count_key);
       add_post_meta($postID, $count_key, '0');
-    }else{
+  }
+  return $count . ' Views';
+}
+
+//記事閲覧数を保存する
+function setPostViews($postID) {
+  $count_key = 'post_views_count';
+  $count = get_post_meta($postID, $count_key, true);
+  if ($count == '') {
+      $count = 0;
+      delete_post_meta($postID, $count_key);
+      add_post_meta($postID, $count_key, '1'); // 初回閲覧時に1を設定
+  } else {
       $count++;
       update_post_meta($postID, $count_key, $count);
-    }
   }
+}
+
+// 投稿が表示されるたびに閲覧数を更新
+add_action('wp_head', function() {
+  if (is_single()) {
+      setPostViews(get_the_ID());
+  }
+});
+
   remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
   add_filter('manage_posts_columns', 'posts_column_views');
   add_action('manage_posts_custom_column', 'posts_custom_column_views', 5, 2);
