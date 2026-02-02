3a538f83d6fcd08fa291add8fdc07b42811dac48
HarutoMizuno <koretteZIYUUya2024@gmail.com>
2026-02-02 16:43:37 +0900

    change-202
    smacss化に伴い、function.phpや各種必要なphpファイルの詳細を変更

--- page.php
+++ page.php
@@ -1,99 +1,98 @@
-
 <?php get_header();?>
-        <div class="empty"></div>
-        <main>
-            <article>
-                
-                <div class="content">
-                      <?php the_content(); wp_link_pages();?>
-                </div>
-            </article>
-        
-
-        <section class="wantToRead">
-        <h2 class="wantToRead_title">#あわせて読みたいTopics</h2>
-        <div class="feature-wrapper">
-        <?php
-        // 現在の投稿のタグを取得
-        $current_tags = wp_get_post_tags(get_the_ID());
-
-        if ($current_tags) {
-            // タグIDを取得
-            $tag_ids = array_map(function($tag) {
-                return $tag->term_id;
-            }, $current_tags);
-
-            // タグに関連する記事を取得
-            $args = array(
-                'tag__in' => $tag_ids, // 現在のタグに一致する記事
-                'post__not_in' => array(get_the_ID()), // 現在の投稿を除外
-                'posts_per_page' => 3, // 最大3件
-                'orderby' => 'date', // 日付順
-                'order' => 'DESC', // 新しい順
-            );
-        } else {
-            // タグがない場合は新着順で取得
-            $args = array(
-                'post__not_in' => array(get_the_ID()), // 現在の投稿を除外
-                'posts_per_page' => 3, // 最大3件
-                'orderby' => 'date', // 日付順
-                'order' => 'DESC', // 新しい順
-            );
-        }
-
-        $query = new WP_Query($args);?>
-
-            <div class="feature-wrapper">
-                <?php if ($query->have_posts()): ?>
-                    <?php while ($query->have_posts()): $query->the_post(); ?>
-                        <a href="<?php the_permalink(); ?>" class="feature">
-                            <div class="Thumbnail">
-                                <?php the_post_thumbnail('post_thumbnails'); ?> 
-                            </div>
-                            <p class="feature_text"><?php the_title(); ?></p>
-                            <div class="feature_text__small">
-                                <p><?php echo get_the_date(); ?></p>
-                                <div class="feature_textAcount"> 
-                                    <img class="feature_textIcon" src="<?php echo get_template_directory_uri(); ?>/img/GakusonLogo.png">
-                                    <p><?php echo get_the_author(); ?></p>
-                                </div>
-                            </div>
-                        </a>
-                    <?php endwhile; ?>
-                <?php else: ?>
-                    <p>投稿がありません</p>
-                <?php endif; ?>
+        <div class="l-empty"></div>
+        <main id="main" class="l-main">
+            <div class="backBoard">
+                <div class="backBoard_item backBoard_item__1"></div>
             </div>
-            <?php wp_reset_postdata(); // クエリをリセット ?>
-        </div>
-        </section>
+            <div class="l-mainContent">
+                <article class="l-article">
+                    <div class="content">
+                        <?php the_content(); wp_link_pages();?>
+                    </div>
+                </article>
+                <section class="l-wantToRead">
+                    <h2 class="wantToRead_title">#あわせて読みたいTopics</h2>
+                    <?php
+                        // 現在の投稿のタグを取得
+                        $current_tags = wp_get_post_tags(get_the_ID());
+                        if ($current_tags) {
+                            // タグIDを取得
+                            $tag_ids = array_map(function($tag) {
+                                return $tag->term_id;
+                            }, $current_tags);
 
-        <section class="Hashtag">
-            <div class="Hashtag_content">
-            <div class="Hashtag_title">
-            <img class="Hashtag_titleIcon" src="<?php echo get_template_directory_uri();?>/icon/線画のフォルダアイコン 2.png">
-            <h2 class="Hashtag_titleText">#ハッシュタグ一覧</h2>
-           </div>
-           <?php   function custom_wp_tag_cloud($tag_string) {
-            // liタグにclass="Hashtag_text"を適用し、aタグの余分なclassを削除
-            $tag_string = preg_replace('/<li(.*?)>/', '<li class="Hashtag_text"$1>', $tag_string);
-            $tag_string = preg_replace('/<a (.*?)class="(.*?)"(.*?)>/', '<a $1$3>', $tag_string);
-            return $tag_string;
-        }
-        add_filter('wp_tag_cloud', 'custom_wp_tag_cloud');?>
+                            // タグに関連する記事を取得
+                            $args = array(
+                                'tag__in' => $tag_ids, // 現在のタグに一致する記事
+                                'post__not_in' => array(get_the_ID()), // 現在の投稿を除外
+                                'posts_per_page' => 3, // 最大3件
+                                'orderby' => 'date', // 日付順
+                                'order' => 'DESC', // 新しい順
+                            );
+                        } else {
+                            // タグがない場合は新着順で取得
+                            $args = array(
+                                'post__not_in' => array(get_the_ID()), // 現在の投稿を除外
+                                'posts_per_page' => 3, // 最大3件
+                                'orderby' => 'date', // 日付順
+                                'order' => 'DESC', // 新しい順
+                            );
+                        }
 
-        <ul class="Hashtag-wrapper">
-            <?php wp_tag_cloud(array(
-                'format' => 'list', // li形式
-                'smallest' => 1,    // 最小フォントサイズ（無効化）
-                'largest' => 1,     // 最大フォントサイズ（無効化）
-                'unit' => 'em',     // サイズ単位（無効化目的）
-                'orderby' => 'count',
-                'order' => 'DESC',
-                'number' => 0,
-            )); ?>
-        </div>
-        </section>
+                        $query = new WP_Query($args);
+                    ?>
+
+                    <div class="feature-wrapper">
+                        <?php if ($query->have_posts()): ?>
+                            <?php while ($query->have_posts()): $query->the_post(); ?>
+                                <a href="<?php the_permalink(); ?>" class="feature">
+                                    <div class="Thumbnail">
+                                        <?php the_post_thumbnail('post_thumbnails'); ?> 
+                                    </div>
+                                    <p class="feature_text"><?php the_title(); ?></p>
+                                    <div class="feature_text__small">
+                                        <p><?php echo get_the_date(); ?></p>
+                                        <div class="feature_textAcount"> 
+                                            <img class="feature_textIcon" src="<?php echo get_template_directory_uri(); ?>/img/GakusonLogo.png">
+                                            <p><?php echo get_the_author(); ?></p>
+                                        </div>
+                                    </div>
+                                </a>
+                            <?php endwhile; ?>
+                        <?php else: ?>
+                            <p>投稿がありません</p>
+                        <?php endif; ?>
+                    </div>
+                    <?php wp_reset_postdata(); // クエリをリセット ?>
+                </section>
+
+                <section class="l-Hashtag">
+                    <div class="Hashtag_content">
+                        <div class="Hashtag_title">
+                            <img class="Hashtag_titleIcon" src="<?php echo get_template_directory_uri();?>/icon/線画のフォルダアイコン 2.png">
+                            <h2 class="Hashtag_titleText">#ハッシュタグ一覧</h2>
+                        </div>
+                        <?php   function custom_wp_tag_cloud($tag_string) {
+                            // liタグにclass="Hashtag_text"を適用し、aタグの余分なclassを削除
+                            $tag_string = preg_replace('/<li(.*?)>/', '<li class="Hashtag_text"$1>', $tag_string);
+                            $tag_string = preg_replace('/<a (.*?)class="(.*?)"(.*?)>/', '<a $1$3>', $tag_string);
+                            return $tag_string;
+                        }
+                        add_filter('wp_tag_cloud', 'custom_wp_tag_cloud');?>
+
+                        <div class="wp_tag_cloud-wrapper">
+                            <?php wp_tag_cloud(array(
+                                'format' => 'list', // li形式
+                                'smallest' => 1,    // 最小フォントサイズ（無効化）
+                                'largest' => 1,     // 最大フォントサイズ（無効化）
+                                'unit' => 'em',     // サイズ単位（無効化目的）
+                                'orderby' => 'count',
+                                'order' => 'DESC',
+                                'number' => 0,
+                            )); ?>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </main>
-        <?php get_footer();?>
-        
+        <?php get_footer();?>       
--- tag.php
+++ tag.php
@@ -1,63 +1,59 @@
 <?php get_header();?>
 <!-- singleをベースに作成開始 sidebarはそのままに、左側すべてに記事を並べる機能を実装 -->
-        <div class="empty"></div>
-        <main>
-            
-            <div class="mainBody">
-                <section class="article">
-                <h2 class="article_title">タグ：『<?php single_cat_title(); ?>』の記事一覧</h1></h2>
+        <div class="l-empty"></div>
+        <main id="main" class="l-main">
+            <div class="l-mainBody">
+                <section class="l-article">
+                    <h2 class="article_title">タグ：『<?php single_cat_title(); ?>』の記事一覧</h1></h2>
                     <div class="feature-wrapper">
                         <?php if( have_posts() ):?>
                             <?php while( have_posts() ):the_post();?>
-                            <a href="<?php the_permalink();?>" class="feature">
-                                <div class="Thumbnail">
-                                <?php the_post_thumbnail('post_thumbnails');?> 
-                                </div>
-                                <h3 class="feature_text"><?php the_title();?></h3>
-                                <div class="feature_text__small">
-                                    <p><?php echo get_the_date();?></p>
-                                    <div class="feature_textAcount"> 
-                                    <img class="feature_textIcon" src="<?php echo get_template_directory_uri();?>/img/GakusonLogo.png">
-                                    <p> <?php echo get_the_author();?></p>
+                                <a href="<?php the_permalink();?>" class="feature">
+                                    <div class="Thumbnail">
+                                        <?php the_post_thumbnail('post_thumbnails');?> 
                                     </div>
-                                </div>
-                            </a>
+                                    <h3 class="feature_text"><?php the_title();?></h3>
+                                    <div class="feature_text__small">
+                                        <p><?php echo get_the_date();?></p>
+                                        <div class="feature_textAcount"> 
+                                            <img class="feature_textIcon" src="<?php echo get_template_directory_uri();?>/img/GakusonLogo.png">
+                                            <p><?php echo get_the_author();?></p>
+                                        </div>
+                                    </div>
+                                </a>
                             <?php endwhile;else:?>
                             <p>投稿がありません</p>
-                            <?php endif;?>    
+                        <?php endif;?>    
                     </div>
-
                 </section>
                 <?php get_sidebar();?>
             </div>
-
-
-            <section class="Hashtag">
-            <div class="Hashtag_content">
-            <div class="Hashtag_title">
-            <img class="Hashtag_titleIcon" src="<?php echo get_template_directory_uri();?>/icon/線画のフォルダアイコン 2.png">
-            <h2 class="Hashtag_titleText">#ハッシュタグ一覧</h2>
-           </div>
-           <?php   function custom_wp_tag_cloud($tag_string) {
-            // liタグにclass="Hashtag_text"を適用し、aタグの余分なclassを削除
-            $tag_string = preg_replace('/<li(.*?)>/', '<li class="Hashtag_text"$1>', $tag_string);
-            $tag_string = preg_replace('/<a (.*?)class="(.*?)"(.*?)>/', '<a $1$3>', $tag_string);
-            return $tag_string;
-        }
-        add_filter('wp_tag_cloud', 'custom_wp_tag_cloud');?>
-
-        <ul class="Hashtag-wrapper">
-            <?php wp_tag_cloud(array(
-                'format' => 'list', // li形式
-                'smallest' => 1,    // 最小フォントサイズ（無効化）
-                'largest' => 1,     // 最大フォントサイズ（無効化）
-                'unit' => 'em',     // サイズ単位（無効化目的）
-                'orderby' => 'count',
-                'order' => 'DESC',
-                'number' => 0,
-            )); ?>
-        </div>
-        </section>
+            <section class="l-Hashtag">
+                <div class="Hashtag_content">
+                    <div class="Hashtag_title">
+                        <img class="Hashtag_titleIcon" src="<?php echo get_template_directory_uri();?>/icon/線画のフォルダアイコン 2.png">
+                        <h2 class="Hashtag_titleText">#ハッシュタグ一覧</h2>
+                    </div>
+                    <?php   function custom_wp_tag_cloud($tag_string) {
+                        // liタグにclass="Hashtag_text"を適用し、aタグの余分なclassを削除
+                        $tag_string = preg_replace('/<li(.*?)>/', '<li class="Hashtag_text"$1>', $tag_string);
+                        $tag_string = preg_replace('/<a (.*?)class="(.*?)"(.*?)>/', '<a $1$3>', $tag_string);
+                        return $tag_string;
+                    }
+                    add_filter('wp_tag_cloud', 'custom_wp_tag_cloud');?>
+                    <div class="wp_tag_cloud-wrapper">
+                        <?php wp_tag_cloud(array(
+                            'format' => 'list', // li形式
+                            'smallest' => 1,    // 最小フォントサイズ（無効化）
+                            'largest' => 1,     // 最大フォントサイズ（無効化）
+                            'unit' => 'em',     // サイズ単位（無効化目的）
+                            'orderby' => 'count',
+                            'order' => 'DESC',
+                            'number' => 0,
+                        )); ?>
+                    </div>
+                </div>
+            </section>
         </main>
         <?php get_footer();?>
    
