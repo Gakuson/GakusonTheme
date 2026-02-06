2f3ebc393d09745b317cdea4cdfeabf395116890
HarutoMizuno <koretteZIYUUya2024@gmail.com>
2026-02-06 23:43:48 +0900

    change-2-06
    All in one seoを運用するにあたってindex.phpの内容を改変。またアニメーションの運用にあたってjsディレクトリ内のファイルを改変。

--- js/navbutton.js
+++
@@ -1,8 +0,0 @@
-jQuery('.headerMain_spMenuImg').click(function(){
-    if(jQuery('.dropdown-wrapper').is(':hidden')){
-    jQuery('.dropdown-wrapper').slideDown();
-}else{
-    jQuery('.dropdown-wrapper').slideUp();
-}
-})
-
